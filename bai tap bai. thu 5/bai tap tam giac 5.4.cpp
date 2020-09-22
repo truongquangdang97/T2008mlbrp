8817d8e8785d2ee1ca7aad027b8cd9e9c192c0e9
truongquangdang97 <70947896+truongquangdang97@users.noreply.github.com>
2020-09-22 08:21:56 +0700

    bai  tap buoi 5.4
    bai  tap buoi 5

---
+++ bai tap bai. thu 5/bai tap tam giac 5.4.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+
+int main(){
+    int a,b,c,p;
+    do{
+    printf("nhap a:");
+    scanf("%d",&a);
+    printf("nhap b:");
+    scanf("%d",&b);
+    printf("nhap c:");
+    scanf("%d",&c);
+    }while (a+b<c||a+c<c||b+c<a);{
+        p=a+b+c;{
+            
+        }
+    
+    }printf(" chu vi tam giac %d ",p);
+
+
+}
